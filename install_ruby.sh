e50483a0f799e3b73c0e1013073ceeb99e84ae37
daniilperestoronin <perestoronin.daniil@gmail.com>
2019-03-31 23:05:00 +0300

    Update scripts

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-apt update && \
-apt install -y ruby-full ruby-bundler build-essential
+
+sudo apt update && \
+sudo apt install -y ruby-full ruby-bundler build-essential
 ruby -v
 bundler -v
