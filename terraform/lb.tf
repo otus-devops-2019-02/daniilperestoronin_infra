c55cc35f8bbd27eaa74f99da56b3caca52170cea
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2019-04-02 17:47:19 +0300

    Add Load Balancer

---
+++ terraform/lb.tf
@@ -0,0 +1,27 @@
+resource "google_compute_http_health_check" "reddit_healthcheck" {
+  name               = "reddit-healthcheck"
+  port               = "9292"
+  request_path       = "/"
+  timeout_sec        = 1
+  check_interval_sec = 1
+}
+
+resource "google_compute_target_pool" "reddit_pool" {
+  name = "reddit"
+
+  instances = [
+    "${google_compute_instance.app.*.self_link}",
+  ]
+
+  health_checks = [
+    "${google_compute_http_health_check.reddit_healthcheck.name}",
+  ]
+}
+
+resource "google_compute_forwarding_rule" "reddit_forwarding_rule" {
+  name = "reddit-forwarding-rule"
+
+  target = "${google_compute_target_pool.reddit_pool.self_link}"
+
+  port_range = "9292"
+}
