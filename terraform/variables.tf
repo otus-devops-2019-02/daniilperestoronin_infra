98a53868eacf1348bbcef6ae65a92c833ac4ea0c
daniilperestoronin <perestoronin.daniil@gmail.com>
2019-04-24 07:16:46 +0300

    Divide main.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -33,3 +33,13 @@ variable disk_image {
 variable "node_count" {
   default = "1"
 }
+
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default     = "reddit-app-base"
+}
+
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default     = "reddit-db-base"
+}
