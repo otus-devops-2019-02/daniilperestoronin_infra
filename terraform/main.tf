62f1e16dfc1a52f3abc9af21d3270058b134e4ad
daniilperestoronin <perestoronin.daniil@gmail.com>
2019-04-25 22:49:54 +0300

    Add modules

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,19 @@
-terraform {
-  # Версия terraform  # required_version = "0.11.13"
-}
-
 provider "google" {
-  # Версия провайдера
   version = "2.0.0"
-
-  # ID проекта
   project = "${var.project}"
   region  = "${var.region}"
+}
+
+module "app" {
+  source          = "modules/app"
+  public_key_path = "${var.public_key_path}"
+  zone            = "${var.zone}"
+  app_disk_image  = "${var.app_disk_image}"
+}
+
+module "db" {
+  source          = "modules/db"
+  public_key_path = "${var.public_key_path}"
+  zone            = "${var.zone}"
+  db_disk_image   = "${var.db_disk_image}"
 }
